e017a33642ed099af3a8218a72a57c6d5a1cdf41
Zahan Malkani <zahanm@gmail.com>
2012-11-14 16:40:18 -0800

    virtualenv

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -15,14 +15,16 @@ def iterdata(source):
 
 
 
-def explore():
+def bagofwords():
   trainfile = "../../Tweet-Data/Romney-Labeled.csv"
   for data in iterdata(trainfile):
-    data["Tweet"]
+    words = data["Tweet"].split(r"\s+")
+    for word in words:
+      vocab[ word ] += 1
     classes[ data["Answer1"] ] += 1
     classes[ data["Answer2"] ] += 1
     classes[ data["Answer"] ] += 1
   print classes
 
 if __name__ == "__main__":
-  explore()
+  bagofwords()
