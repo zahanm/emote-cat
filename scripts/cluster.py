3a493a849020e95c4beab7137f6185f3a53fb41a
Zahan Malkani <zahanm@gmail.com>
2012-11-15 14:05:01 -0800

    bernoulli features

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -5,6 +5,7 @@ import functools
 
 import nltk
 import emoticons
+import numpy as np
 
 from crossval import KFoldData
 
@@ -18,16 +19,30 @@ def not_in_stoplist(t):
 def to_lower(s):
   return s.lower()
 
-def train(data):
+def features_bernoulli(data):
   porter = nltk.PorterStemmer()
+  numtraining = 0
   for tweetinfo in data.train():
     tokens = transform( tweetinfo["Tweet"] )
     for tok in tokens:
       vocab[ tok ] += 1
     classes[ tweetinfo["Answer1"] ] += 1
-    classes[ tweetinfo["Answer2"] ] += 1
-    classes[ tweetinfo["Answer"] ] += 1
-  print classes
+    numtraining += 1
+  numtoks = len(vocab)
+  featureMap = {}
+  for j, tok in enumerate(vocab.iterkeys()):
+    featureMap[tok] = j
+  labelMap = {}
+  for j, label in enumerate(classes.iterkeys()):
+    labelMap[label] = j
+  features = np.zeros((numtraining, numtoks), dtype=np.bool)
+  labels = np.zeros((numtraining), dtype=np.uint8)
+  for i, tweetinfo in enumerate(data.train()):
+    tokens = transform( tweetinfo["Tweet"] )
+    for tok in tokens:
+      features[i, featureMap[tok]] = True
+    labels[i] = labelMap[ tweetinfo["Answer1"] ]
+  return (features, featureMap, labels, labelMap)
 
 porter = nltk.PorterStemmer()
 def transform(text):
@@ -51,4 +66,4 @@ def transform(text):
 
 if __name__ == "__main__":
   data = KFoldData("../Tweet-Data/Romney-Labeled.csv")
-  train(data)
+  features, featureMap, labels, labelMap = features_bernoulli(data)
