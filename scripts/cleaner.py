ce7ae34c91a4f391c704f7025a65b9080c3a8686
Zahan Malkani <zahanm@gmail.com>
2012-12-07 12:01:59 -0800

    fk this, i'm just removing stuff now

--- scripts/cleaner.py
+++ scripts/cleaner.py
@@ -5,6 +5,7 @@ import gzip
 import re
 import itertools
 from datetime import datetime
+import random
 
 from datareaders import TSVReader
 
@@ -17,10 +18,56 @@ def clean(inp_fname):
       # header line
       header = inp.next().strip().split("\t")
       out.write("\t".join(header) + "\n")
+      length = len(header)
+      numskipped = 0
+      numtotal = 0
       # other lines
       for i, line in enumerate(inp):
+        numtotal += 1
         items = re.split(r"\t", line.strip())
-        if len(items) != len(header):
+        if len(items) != length:
+          numskipped += 1
+          continue
+          """
+          screw this
+          """
+          if len(items) <= 1:
+            continue
+          elif len(items) == 2 * length:
+            out.write("\t".join(items[ :length / 2]) + "\n")
+            out.write("\t".join(items[length / 2: ]) + "\n")
+          else:
+            expected = [ r"^\w+$", r"^2012-11-06 \d{2}:\d{2}:\d{2}$", r"^\d+$", r"^[\w\-\s@]+$"]
+            expected_date = "2012-11-06 "
+            progress = 0
+            j = 0
+            unfixable = False
+            while j < len(items) and not unfixable:
+              item = items[j]
+              if progress >= len(header):
+                unfixable = True
+              elif not re.match(expected[progress], item):
+                if progress == 0:
+                  print "line number: {}".format(i+2)
+                  print "items: {}".format(items)
+                  name = raw_input("name: ")
+                  items.insert(j, name.strip())
+                elif progress == 1:
+                  print "line number: {}".format(i+2)
+                  print "items: {}".format(items)
+                  time = raw_input("date {} time: ".format(expected_date))
+                  items.insert(j, time.strip())
+                elif progress == 2:
+                  items.insert(j, str(random.randint(200000000000000000, 300000000000000000)))
+                else:
+                  unfixable = True
+                j += 1
+              progress += 1
+              j += 1
+            if not unfixable and len(items) == len(header):
+              print "writing on " + str(i+2) + " => " + "\t".join(items)
+              out.write("\t".join(items) + "\n")
+              continue
           print "line number: {}".format(i+2)
           print "items: {}".format(items)
           skip = raw_input("skip? (y/n): ")
@@ -29,28 +76,21 @@ def clean(inp_fname):
           else:
             print "No skip"
             sys.exit(2)
-        row = {}
-        for key, val in itertools.izip(header, items):
-          if re.match(r"tweet_?id", key, re.I):
-            row["TweetId"] = val
-          elif re.match(r"tweet|text", key, re.I):
-            row["Tweet"] = val
-          elif re.match(r"label|class", key, re.I):
-            row["Answer"] = val
-            row["Answer1"] = val
-            row["Answer2"] = val
-          elif re.match(r"(date|time)+", key, re.I):
-            row["Datetime"] = datetime.strptime(val, "%Y-%m-%d %H:%M:%S")
-          elif re.match(r"author|tweeter", key, re.I):
-            row["Author"] = val
-          else:
+        expected_names = ["name", "date", "id", "tweet"]
+        expected = [ r"^\w+$", r"^2012-11-06 \d{2}:\d{2}:\d{2}$", r"^\d+$", r".+"]
+        for name, pat, item in itertools.izip(expected_names, expected, items):
+          if not re.match(pat, item):
             print "line number: {}".format(i+2)
-            print "line: {}".format(line.rstrip())
-            print "Bad Line"
-            sys.exit(2)
-            # print "WARN: Are you sure that the datafile has a header line?"
+            print "items: {}".format(items)
+            print "not matching {} : {}".format(name, item)
+            skip = raw_input("exit? (y/n): ")
+            if re.match(r"y(es)?", skip, re.I):
+              sys.exit(2)
+          """
+          all done
+          """
         out.write(line)
-  print "Done"
+  print "Skipped {} out of {}".format(numskipped, numtotal)
 
 if __name__ == '__main__':
   if len(sys.argv) != 2:
