aeb4a1723f7118018a0d16791da00d5ec94751f0
Zahan Malkani <zahanm@gmail.com>
2012-12-15 13:35:54 +0530

    update nb

--- scripts/othermodels.py
+++ scripts/othermodels.py
@@ -13,12 +13,11 @@ class NaiveBayes(dict):
       self[label] = {}
       counts = np.sum(features[labels == label, :], 0)
       total = float(np.sum(counts))
-      if total == 0 or overalltotal == 0:
+      if total == 0:
         self[label]["probs"] = counts
-        self[label]["prior"] = total
       else:
         self[label]["probs"] = counts / total
-        self[label]["prior"] = total / overalltotal
+      self[label]["prior"] = total / overalltotal
     return self
 
   def apply(self, datum):
